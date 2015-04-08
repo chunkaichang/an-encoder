bf28b8e02658f10e761a5e51ae27ed9e359e49b1
Norman Rink <norman.rink@tu-dresden.de>
2015-04-08 12:01:55 +0200

    Also handle multi-dimensional array initializers in the "GlobalsEncoder".

--- encode/src/GlobalsEncoder.cpp
+++ encode/src/GlobalsEncoder.cpp
@@ -16,11 +16,45 @@ namespace {
 
     static char ID;
   private:
+    Constant *buildEncodedConstant(Constant *);
+
     Coder *C;
   };
 }
 
 char GlobalsEncoder::ID = 0;
+
+Constant *GlobalsEncoder::buildEncodedConstant(Constant *init) {
+  Constant *result = init;
+  Type *ty = init->getType();
+
+  if (ty->isIntegerTy()) {
+    if (const ConstantInt *ci = dyn_cast<ConstantInt>(init)) {
+      uint64_t res = ci->getValue().getLimitedValue() * C->getA();
+      // Programs for encoding are expected to operate on 64bit integers only,
+      // so we also only handle 64bit integers here: (The main reason for not
+      // having an assertion here is that strings, i.e. arrays with element
+      // type 'i8', must not be encoded; yet strings are very common.)
+      if (ci->getType() != C->getInt64Type())
+        return result;
+
+      result = ConstantInt::get(ci->getType(), res);
+    }
+  } else if (ty->isArrayTy()) {
+    ArrayType *aty = dyn_cast<ArrayType>(ty);
+    SmallVector<Constant*, 32> newElements;
+
+    for (unsigned i = 0; i < aty->getNumElements(); i++) {
+      Constant *newElt, *oldElt = init->getAggregateElement(i);
+      newElt = buildEncodedConstant(oldElt);
+      newElements.push_back(newElt);
+    }
+
+    result = ConstantArray::get(aty, newElements);
+  }
+
+  return result;
+}
 
 bool GlobalsEncoder::runOnModule(Module &M) {
   bool modified = false;
@@ -31,34 +65,8 @@ bool GlobalsEncoder::runOnModule(Module &M) {
       continue;
 
     Constant *init = GV->getInitializer();
-    if (const ConstantInt *ci = dyn_cast<ConstantInt>(init)) {
-      uint64_t res = ci->getValue().getLimitedValue() * C->getA();
-      assert(ci->getType() == C->getInt64Type() &&
-             "Unexpected non-64bit integer type");
-      ConstantInt *enc = ConstantInt::get(ci->getType(), res);
-      GV->setInitializer(enc);
-
-      modified = true;
-    } else if (const ConstantDataArray *cda = dyn_cast<ConstantDataArray>(init)) {
-      // We only handle arrays whose elements are 64bit integers: (The main reason
-      // for doing this is that strings, i.e. arrays with element type 'i8', must
-      // not be encoded; yet strings are very common.)
-      if (cda->getElementType() != C->getInt64Type())
-        continue;
-
-      assert(cda->getElementType() == C->getInt64Type() &&
-             "Unexpected non-64bit integer type");
-
-      SmallVector<uint64_t, 32> result;
-      for (unsigned i = 0; i < cda->getNumElements(); i++) {
-        uint64_t res = cda->getElementAsInteger(i) * C->getA();
-        result.push_back(res);
-      }
-      LLVMContext &ctx = M.getContext();
-      GV->setInitializer(ConstantDataArray::get(ctx, result));
-
-      modified = true;
-    }
+    GV->setInitializer(buildEncodedConstant(init));
+    modified = true;
   }
 
   return modified;
