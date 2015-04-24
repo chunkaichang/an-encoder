0980d05a1fd74e42885a0f3aab65d9b0ca9f50fc
Norman Rink <norman.rink@tu-dresden.de>
2015-04-24 14:19:10 +0200

    Added library functions for writing (intermediate) results from test cases to a (binary) file.
    This new method of logging results is intended to supersede the
    previous logging with hashes/checksums. When relying on hashes or
    checksums to determine whether a program has executed correctly,
    we might miss errors that do not affect the final value of the
    checksum. (In practise, however, such errors are probably very,
    very rare.)
    
    CAUTION: It is still possible that (intermediate) results that are
    written to a file are correct despite the fact that an error has
    occurred. This is particularly likely if the granularity at which
    intermediate results are written out is very coarse. (In the worst
    case, e.g., only one final result is written out.)

--- encode/tests/mylibs/mycheck.c
+++ encode/tests/mylibs/mycheck.c
@@ -1,8 +1,11 @@
 
 #include <stdint.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 static __uint128_t checksum;
+static FILE *fp_checksum;
 
 __uint128_t __cs_reset() {
   checksum = 0;
@@ -25,4 +28,27 @@ void __cs_msg(void) {
   lo = (cs << 64) >> 64;
   fprintf(stderr, "hi(checksum)=0x%016lX\n", hi);
   fprintf(stderr, "lo(checksum)=0x%016lX\n", lo);
+}
+
+void __cs_fopen(int argc, char** argv) {
+  const char *file = "cs.out";
+  unsigned i = 0;
+
+  while (i < argc) {
+    if (!strcmp("--cso", argv[i])) {
+      if (argc <= i+1) exit(1);
+      file = argv[i+1];
+      break;
+    }
+    ++i;
+  }
+  fp_checksum = fopen(file, "w");
+}
+
+size_t __cs_facc(uint64_t x) {
+  return fwrite(&x, sizeof(__uint64_t), 1, fp_checksum);
+}
+
+void __cs_fclose() {
+  fclose(fp_checksum);
 }
--- encode/tests/mylibs/mycheck.h
+++ encode/tests/mylibs/mycheck.h
@@ -2,10 +2,15 @@
 #define __MYCHECK_H__
 
 #include <stdint.h>
+#include <stdlib.h>
 
 __uint128_t __cs_reset();
 __uint128_t __cs_get();
 __uint128_t __cs_acc(__uint128_t x);
 void        __cs_msg(void);
+
+void        __cs_fopen(int argc, char** argv);
+size_t      __cs_facc(uint64_t x);
+void        __cs_fclose();
 
 #endif /* __MYCHECK_H__ */
