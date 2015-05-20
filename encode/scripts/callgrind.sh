daf24b9022a01d571f0b850d537381e49cc9a572
Norman Rink <norman.rink@tu-dresden.de>
2015-05-20 17:23:42 +0200

    Simple scripts for running 'valgrind' tools on performance tests.

---
+++ encode/scripts/callgrind.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+DEBUG="Release"
+SOURCE_PATH="."
+LENGTH=100
+
+while getopts "ds:l:p:e:n:" opt; do
+	case $opt in
+		d) DEBUG="Debug"
+			;;
+		s) SOURCE_PATH=$OPTARG
+			;;
+		l) LENGTH=$OPTARG
+			;;
+		p) PLAIN_TEST=$OPTARG
+			;;
+		e) ENCODED_TEST=$OPTARG
+			;; 
+		n) NAME=$OPTARG
+			;;
+	esac
+done
+
+cmake -DLENGTH_PERF=${LENGTH} -DCMAKE_BUILD_TYPE=${DEBUG} -L ${SOURCE_PATH}
+make -j4
+valgrind --tool=callgrind --simulate-cache=yes \
+	--callgrind-out-file=callgrind.${NAME}.plain.${DEBUG}.${LENGTH}.out \
+	${PLAIN_TEST}
+valgrind --tool=callgrind --simulate-cache=yes \
+	--callgrind-out-file=callgrind.${NAME}.encoded.${DEBUG}.${LENGTH}.out \
+	${ENCODED_TEST}
